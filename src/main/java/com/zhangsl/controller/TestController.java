dc32b1fa95bc4c9ec92ffd097752326b5ebfbb78
“Zhangsl <1083065285@qq.com>
2018-02-03 17:33:03 +0800

    Spring MVC开发环境搭建与配置,mybatis generator使用

---
+++ src/main/java/com/zhangsl/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.zhangsl.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Zhangsl on 2018/2/3.
+ */
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello.json")
+    @ResponseBody
+    public String hello() {
+        log.info("hello");
+        //throw new PermissionException("test exception");
+        // return JsonData.success("hello, permission");
+        return "hello permisson";
+    }
+}
---
+++ src/main/java/com/zhangsl/dao/TestDao.java
@@ -0,0 +1,7 @@
+package com.zhangsl.dao;
+
+/**
+ * Created by Zhangsl on 2018/2/3.
+ */
+public interface TestDao {
+}

cee2ff3053d0f7aaed7380f392e870c590e86113
“Zhangsl <1083065285@qq.com>
2018-02-09 14:20:59 +0800

    使用mybatis generator工具自动生成dao,bean代码

---
+++ src/main/java/com/zhangsl/dao/SysLogMapper.java
@@ -0,0 +1,20 @@
+package com.zhangsl.dao;
+
+import com.zhangsl.model.SysLog;
+import com.zhangsl.model.SysLogWithBLOBs;
+
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysLogWithBLOBs record);
+
+    int insertSelective(SysLogWithBLOBs record);
+
+    SysLogWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysLogWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(SysLogWithBLOBs record);
+
+    int updateByPrimaryKey(SysLog record);
+}
